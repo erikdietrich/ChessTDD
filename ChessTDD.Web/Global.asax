a54ad7edfb3c8fc6c5c798a8aff8000853bfc7fb
Erik Dietrich <erik@daedtech.com>
2017-02-05 11:36:32 -0600

    Added a web project front end.

---
+++ ChessTDD.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChessTDD.Web.MvcApplication" Language="C#" %>
---
+++ ChessTDD.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChessTDD.Web.Startup))]
+namespace ChessTDD.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
